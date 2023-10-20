fae300e70c5922018bd0ab695269c47e063169bd
erfan mahmoudi <erfanmahmudi1383@gmail.com>
2023-10-20 22:54:58 +0330

    some minor changes added as well

--- src/main/java/com/roomreservation/management/security/GlobalExceptionHandler.java
+++ src/main/java/com/roomreservation/management/security/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.roomreservation.management.security;
 
+import com.roomreservation.management.support.ErrorResponse;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
@@ -48,5 +50,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(LoginDeniedException.class)
     public ResponseEntity<String> handleLoginDeniedException(LoginDeniedException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Login Denied");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorResponse illegalArgument(IllegalArgumentException exception) {
+        return ErrorResponse.error(exception.getMessage());
     }
 }
---
+++ src/main/java/com/roomreservation/management/support/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.roomreservation.management.support;
+
+public class ErrorResponse {
+
+    private String message;
+
+    private ErrorResponse() {
+        this("");
+    }
+
+    private ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public static ErrorResponse error(String message) {
+        return new ErrorResponse(message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
